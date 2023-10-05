cb2fd5250cab521d25731873e3b5c3b763180238
AndreAloise <andre.aloise.77@gmail.com>
2023-10-05 09:46:54 -0300

    Implementando um get para o gerar um nome das ordens.

--- workshop_dart/bin/fundamentacao/enums/order_status_enhanced_enum.dart
+++ workshop_dart/bin/fundamentacao/enums/order_status_enhanced_enum.dart
@@ -10,4 +10,17 @@ enum OrderStatus {
   //Overriding the operators to compare one enum with another
   bool operator <(OrderStatus status) => progress < status.progress;
   bool operator >(OrderStatus status) => progress > status.progress;
+
+  String get nome {
+    switch (this) {
+      case OrderStatus.open:
+        return 'Opened Order';
+      case OrderStatus.confirmed:
+        return 'Confirmed Order';
+      case OrderStatus.completed:
+        return 'Completed Order';
+      case OrderStatus.cancelled:
+        return 'Cancelled Order';
+    }
+  }
 }
