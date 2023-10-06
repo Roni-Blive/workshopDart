9dd67d5526c0bf698fcfa15e56025073bc0841ff
AndreAloise <andre.aloise.77@gmail.com>
2023-10-06 17:37:55 -0300

    Ajustando lógica dos métodos

--- workshop_dart/bin/null_safety/function_null_safety.dart
+++ workshop_dart/bin/null_safety/function_null_safety.dart
@@ -1,3 +1,13 @@
-class FunctionNullSafety{
+class FunctionNullSafety {
+  void printName(String name) {
+    print("Hello $name");
+  }
 
+  String? getName() {
+    return null;
+  }
+
+  /*void printNullName() {
+    printName(getName()});
+  }*/
 }
