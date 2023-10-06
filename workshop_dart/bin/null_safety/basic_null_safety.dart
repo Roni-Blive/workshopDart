d9c1284165f6efcdb24043e4779785546af32b0e
AndreAloise <andre.aloise.77@gmail.com>
2023-10-06 15:40:55 -0300

    Criando e implementando classe de exemplo simples de null safety

---
+++ workshop_dart/bin/null_safety/basic_null_safety.dart
@@ -0,0 +1,12 @@
+class BasicNullSafety{
+  String? darkMatter;
+  String planetName = "Earth";
+
+  void addNullToDarkMatter(){
+    darkMatter = null;
+  }
+
+  /*void addNullToPlanetName() {
+    planetName = null;
+  }*/
+}
