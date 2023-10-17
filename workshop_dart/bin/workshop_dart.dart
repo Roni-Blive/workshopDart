f97199e7de1ea35503d205404f263babd7dd4e3f
AndreAloise <andre.aloise.77@gmail.com>
2023-10-17 14:05:21 -0300

    Criando métodos na main que invoca todos os métodos de Fundamentacao e Null-Safety

--- workshop_dart/bin/workshop_dart.dart
+++ workshop_dart/bin/workshop_dart.dart
@@ -1,3 +1,104 @@
+import 'fundamentacao/const_final_static/cfs_examples.dart';
+import 'fundamentacao/dynamic_var/dynamic_var_examples.dart';
+import 'fundamentacao/enums/enum_examples.dart';
+import 'fundamentacao/enums/order_status_enhanced_enum.dart';
+import 'fundamentacao/enums/transportation_enum.dart';
+import 'fundamentacao/interpolation/interpolation_examples.dart';
+import 'fundamentacao/private_public/private_public_examples.dart';
+import 'null_safety/null_safety_examples.dart';
+
 void main(List<String> arguments) {
-  print('Hello world!');
+  /*
+  print('getConstFinalStaticExamples');
+  getConstFinalStaticExamples();
+  print('');
+
+  print('getEnumExamples');
+  getEnumExamples();
+  print('');
+
+  print('getDynamicVarExamples');
+  getDynamicVarExamples();
+  print('');
+
+  print('getPrivatePublicExamples');
+  getPrivatePublicExamples();
+  print('');
+
+  print('getInterpolationExamples');
+  getInterpolationExamples();
+  print('');
+
+  print('getNullSafetyExamples');
+  getNullSafetyExamples();
+  */
+}
+
+void getConstFinalStaticExamples() {
+  ConstFinalStaticExamples exe =
+      ConstFinalStaticExamples("I was born late and missed the cake.");
+  exe.modifyListElement();
+
+  ConstFinalStaticExamples.printStaticProperty();
+  exe.printProperty();
+  print('');
+  ConstFinalStaticExamples exePizza = ConstFinalStaticExamples.initRadius(10);
+  print("Circle area is: ${exePizza.circleArea()} cm.");
+  ConstFinalStaticExamples.staticPizzaArea(15);
+}
+
+void getEnumExamples() {
+  EnumExamples examples = EnumExamples();
+  examples.getSelectedTransportationMessage(Transportation.boat);
+  examples.printOrderStatusComparison(OrderStatus.cancelled);
+}
+
+void getDynamicVarExamples() {
+  DynamicVarExamples dynamicVarExamples = DynamicVarExamples();
+  dynamicVarExamples.tryChangeVarAttributeToDouble();
+  dynamicVarExamples.tryChangeDynamicNumber();
+}
+
+void getPrivatePublicExamples() {
+  PrivatePublicExamples privatePublicExamples = PrivatePublicExamples();
+
+  print(privatePublicExamples.publicProperty);
+  privatePublicExamples.publicProperty = "Now I have a new value";
+  print(privatePublicExamples.publicProperty);
+
+  print(privatePublicExamples.getPrivateProperty);
+}
+
+void getInterpolationExamples() {
+  InterpolationExamples interpolationExamples = InterpolationExamples();
+  interpolationExamples.printVariables();
+  print('');
+  interpolationExamples.printInternalProperties();
+  print('');
+  interpolationExamples.printInternalMethod();
+}
+
+void getNullSafetyExamples() {
+  NullSafetyExamples.getBasicNullSafetyExample();
+  print('');
+  NullSafetyExamples.getNullSafetyAssertionExample();
+  print('');
+  NullSafetyExamples.getLateVariableAfterInitializationExample();
+  print('getLateVariableBeforeInitializationExample will throw an uncatched exception');
+  NullSafetyExamples.getLateVariableBeforeInitializationExample();
+  print('');
+  NullSafetyExamples.getNonNullableListInOneLineExample();
+  print('');
+  NullSafetyExamples.getNonNullableListInManyLinesExample();
+  print('');
+  NullSafetyExamples.getFunctionNullSafetyExample();
+  print('');
+  NullSafetyExamples.getSafeCallCountryInfoExample();
+  print('');
+  NullSafetyExamples.getSafeCallStreetInfoFromNullPersonExample();
+  print('');
+  NullSafetyExamples.getNullAssertionOperatorPerishableProductWithDateExample();
+  print('');
+  NullSafetyExamples
+      .getNullAssertionOperatorPerishableProductWithoutDateExample();
 }
